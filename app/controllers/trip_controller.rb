f684c394bc632ad302f01027b3b4f370cb2a2400
Marcel Amsler <marcel.amsler@gmail.com>
2016-09-17 18:53:03 +0200

    added show detail method for trips

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -8,6 +8,12 @@ class TripController < ActionController::Base
     render json: @trip
   end
 
+  def show
+    car_id = get_car_id_for_user params[:user_id]
+    @trip = Trip.find_by(car_id: car_id, id: params[:id])
+    render json: @trip
+  end
+
   def get_car_id_for_user (user_id)
     if user_id == 1
       87
