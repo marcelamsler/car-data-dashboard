35deab8f0c0d51c7502e12c40ecdac7c87b56647
Marcel Amsler <marcel.amsler@gmail.com>
2016-09-17 02:01:44 +0200

    fixed initializer

--- config/initializers/data-importer.rb
+++ config/initializers/data-importer.rb
@@ -1,4 +1,4 @@
-Dir.foreach('/path/to/dir') do |item|
-  next if item == '.' or item == '..'
-  # do work on real items
-end
+# Dir.foreach('/path/to/dir') do |item|
+#   next if item == '.' or item == '..'
+#   # do work on real items
+# end
