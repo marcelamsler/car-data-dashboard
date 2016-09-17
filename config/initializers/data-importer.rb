e524320249b4103035cbeeb4dde71bb8b8d63526
Raffael Ioannone <rioannon@hsr.ch>
2016-09-17 08:38:10 +0200

    added data importer

--- config/initializers/data-importer.rb
+++ config/initializers/data-importer.rb
@@ -2,3 +2,21 @@
 #   next if item == '.' or item == '..'
 #   # do work on real items
 # end
+require 'csv'
+
+Dir.foreach('./data/csv/') do |item|
+  next if item == '.' or item == '..'
+  puts('Import file: ' + item)
+
+  CSV.foreach('./data/csv/' + item) do |row|
+    if row[0] != 'Latitude'
+      if LogEntrie.column_names.include?(row[4])
+        logentrie = LogEntrie.find_or_create_by(LATITUDE:row[0], LONGITUDE:row[1], RECEIVED_AT:row[2], RECORDED_AT:row[3])
+        logentrie[row[4]] = row[5]
+        logentrie.save!
+      end
+    end
+  end
+  puts LogEntrie.count
+  puts('Finished Import')
+end
