264cfdb6a416cbf04902a8e552df1bd66f76c7c2
Styp <mstypinski@gmail.com>
2016-09-18 02:49:33 +0200

    added colors to table

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -3,8 +3,21 @@ class TripController < ActionController::Base
   def index
     car_id = get_car_id_for_user params[:user_id].to_i
 
-    @trips = Trip.where(car_id: car_id)
+    @trips = Trip.where("car_id = ? AND rpm_mean IS NOT NULL", car_id).map do |trip|
+      new_trip = trip.attributes
+      new_trip[:engine_cond] = get_color_for_engine(trip.rpm_mean)
+      new_trip[:chassis_cond] = get_color_for_chassis(trip.lat_mean)
+      new_trip[:brake_cond] = get_color_for_brake(trip.break_mean)
+      new_trip[:overall_cond] = get_color_for_overall(trip.rpm_mean, trip.lat_mean, trip.break_mean)
+      new_trip
+    end
+
     render json: @trips
+
+  end
+
+  def get_color_for_overall(rpm, lat, brake)
+    'green'
   end
 
   def show
@@ -18,6 +31,36 @@ class TripController < ActionController::Base
       87
     else
       10
+    end
+  end
+
+  def get_color_for_engine (rpm_mean)
+    if rpm_mean < 1484
+      'green'
+    elsif rpm_mean < 1991
+      'orange'
+    else
+      'red'
+    end
+  end
+
+  def get_color_for_brake (brake_mean)
+    if brake_mean < 138
+      'green'
+    elsif brake_mean < 261
+      'orange'
+    else
+      'red'
+    end
+  end
+
+  def get_color_for_chassis (lat_mean)
+    if lat_mean < 22
+      'green'
+    elsif lat_mean < 301
+      'orange'
+    else
+      'red'
     end
   end
 
